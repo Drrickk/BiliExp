66b320d59bea1238f37cc8a88065eea4ff5be8b8
Tim Mingxing <tim.tt24@gmail.com>
2020-09-24 21:54:13 +0800

    update

--- doActivity.py
+++ doActivity.py
@@ -1,32 +1,51 @@
 # -*- coding: utf-8 -*-
 from models.Biliapi import BiliWebApi
-import time, json
+import time
 
-def do_activity(cookieData, activityData):
+def get_activity_lottery():
+    '''获取B站活动抽奖id，生成器'''
+    import requests
+    import re
+    session = requests.session()
+    pat = re.compile('lotteryId\\\":\\\"(.*?)\\"', re.S)
+    def get_id(url):
+        text = session.get(f'https:{url}').text
+        return re.findall(pat, text)[0]
+
+    list = BiliWebApi.activityList()
+    for x in list:
+        try:
+            yield (x["name"], get_id(x["h5_url"]))
+        except:
+            pass
+    session.close()
+
+def do_activity(cookieData, activity_data):
     try:
        biliapi = BiliWebApi(cookieData)
     except Exception as e:
        print(f'登录验证id为{cookieData["DedeUserID"]}的账户失败，原因为({str(e)})，跳过此账户后续所有操作')
        return
-    print(f'id为{cookieData["DedeUserID"]}的账户开始参与活动')
-    for x in activityData:
-        print(f'开始参与 "{x["name"]}" 活动')
-        for y in x["addTimes"]:
-            try:
-                biliapi.activityAddTimes(x["sid"], y) #执行增加抽奖次数操作,一般是分享转发
-            except Exception as e:
-                print(f'增加抽奖次数异常,原因为({str(e)})')
+    print(f'账户({biliapi.getUserName()})开始参与活动抽奖')
+    for x in activity_data:
+        print(f'开始参与 "{x[0]}" 活动')
+        try:
+            biliapi.activityAddTimes(x[1], 3) #执行增加抽奖次数操作
+            biliapi.activityAddTimes(x[1], 4)
+        except Exception as e:
+            print(f'增加抽奖次数异常,原因为({str(e)})')
 
         try:
-            times = biliapi.activityMyTimes(x["sid"])["data"]["times"]
+            times = biliapi.activityMyTimes(x[1])["data"]["times"]
         except Exception as e:
             print(f'获取剩余抽奖次数异常，原因为({str(e)})，在尝试执行1次抽奖')
             times = 1
-        print(f'活动可参与的次数为{times}')
+        else:
+            print(f'活动可参与的次数为{times}')
 
         for ii in range(times):
             try:
-                result = biliapi.activityDo(x["sid"], 1)
+                result = biliapi.activityDo(x[1], 1)
                 print(f'{ii + 1}.', end="")
                 if result["code"]:
                     print(result["message"])
@@ -40,11 +59,10 @@ def main(*args):
     with open('config/config.json','r',encoding='utf-8') as fp:
         configData = json.load(fp)
 
-    with open('config/activity.json','r',encoding='utf-8') as fp:
-        activityData = json.load(fp)
+    activity_data = list(get_activity_lottery())
 
     for x in configData["cookieDatas"]:
-        do_activity(x, activityData)
+        do_activity(x, activity_data)
 
 if __name__=="__main__":
     main()
