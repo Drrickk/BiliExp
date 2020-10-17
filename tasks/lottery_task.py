c3ffcb6793e2185d871530e30e2ad2bb14aea17b
Tim Mingxing <tim.tt24@gmail.com>
2020-10-17 12:44:12 +0800

    增加动态判断

--- tasks/lottery_task.py
+++ tasks/lottery_task.py
@@ -1,6 +1,6 @@
 from models.asyncBiliApi import asyncBiliApi
 from tasks.import_once import now_time
-import logging, json
+import logging, json, asyncio
 
 end_time = now_time - (now_time + 28800) % 86400 + 43200 #当天中午12点
 start_time = end_time - 86400
@@ -24,25 +24,29 @@ async def lottery_task(biliapi: asyncBiliApi,
 
             if 'card' in x:
                 card = json.loads(x["card"])
-                flag = False
-                if 'description' in card["item"]:
-                    text = card["item"]["description"]
-                elif 'content' in card["item"]:
-                    text = card["item"]["content"]
-                else:
-                    text = None
-                if text:
-                    for keyword in task_config["keywords"]:
-                        if keyword in text:
-                            uname = x["desc"]["user_profile"]["info"]["uname"]  #动态的主人的用户名
-                            dyid = x["desc"]["dynamic_id"]
-                            await biliapi.dynamicReplyAdd(oid, task_config["reply"])
-                            await biliapi.dynamicRepostReply(dyid, task_config["repost"])
-                            logging.info(f'{biliapi.name}: 转发关键字动态(用户名:{uname},动态id:{dyid})成功')
-                            flag = True
-                            break
-                if flag:
-                    continue
+                if 'item' in card:
+                    flag = False
+                    if 'description' in card["item"]:
+                        text = card["item"]["description"]
+                    elif 'content' in card["item"]:
+                        text = card["item"]["content"]
+                    else:
+                        text = None
+                    if text:
+                        for keyword in task_config["keywords"]:
+                            if keyword in text:
+                                uname = x["desc"]["user_profile"]["info"]["uname"]  #动态的主人的用户名
+                                dyid = x["desc"]["dynamic_id"]
+                                try:
+                                    await biliapi.dynamicReplyAdd(oid, task_config["reply"])
+                                    await biliapi.dynamicRepostReply(dyid, task_config["repost"])
+                                    logging.info(f'{biliapi.name}: 转发关键字动态(用户名:{uname},动态id:{dyid})成功')
+                                except Exception as e: 
+                                    logging.info(f'{biliapi.name}: 转发关键字动态(用户名:{uname},动态id:{dyid})异常，原因为{str(e)}')
+                                flag = True
+                                break
+                    if flag:
+                        continue
 
             if 'extension' in x and 'lott' in x["extension"]: #若抽奖标签存在
                 uname = x["desc"]["user_profile"]["info"]["uname"]  #动态的主人的用户名
@@ -56,7 +60,7 @@ async def lottery_task(biliapi: asyncBiliApi,
                     logging.info(f'{biliapi.name}: 转发抽奖动态(用户名:{uname},动态id:{dyid})成功')
                     await asyncio.sleep(5)
                 except Exception as e: 
-                    logging.warning(f'{biliapi.name}: 转发抽奖动态(用户名:{uname},动态id:{dyid})失败，原因为{str(e)}')
+                    logging.warning(f'{biliapi.name}: 转发抽奖动态(用户名:{uname},动态id:{dyid})异常，原因为{str(e)}')
 
     except Exception as e: 
         logging.warning(f'{biliapi.name}: 获取动态列表、异常，原因为{str(e)}，跳过转发抽奖动态')
